e873d6416a01bd1591c8df0a6776dbb04e3b1736
jiado <jiado@DESKTOP-KR47POI>
2021-03-08 10:49:20 +0100

    Add some comments, add override keyword for private virtual member

--- design_pattern/prototype/solution2.cpp
+++ design_pattern/prototype/solution2.cpp
@@ -8,6 +8,9 @@
 /*
  * Solution:
  * 2. Use row pointer in Base, and smart pointers elsewhere. Define an extra -impl member function to facilitate the return type as smart pointer
+ *
+ * Name hiding (i.e. when declaring a name in a derived class, this name hides all the symbols with the same name in the base class),
+ * we hide (not override) the clone() member function to return a smart pointer of the exact type we wanted.
  */
 
 #include <memory>
@@ -16,6 +19,11 @@
 class Base{
 public:
 	virtual ~Base(){}
+	std::unique_ptr<Base> clone() const{
+		return std::make_unique<Base>(this->clone_impl());
+	}
+
+private:
 	virtual Base* clone_impl() const = 0;
 };
 
@@ -25,6 +33,7 @@ private:
 	int n;
 public:
 	Derived(int num = 0):n{num}{}
+
 	std::unique_ptr<Derived> clone() const {
 		Derived* d = this->clone_impl();
 		std::unique_ptr<Derived> p = std::make_unique<Derived>(*d);
--- design_pattern/prototype/solution3.cpp
+++ design_pattern/prototype/solution3.cpp
@@ -5,65 +5,78 @@
  *      Author: jiado
  */
 
-#define SOLUTION3
+#define SOLUTION3_1
 
 #include <memory>
 using namespace std;
 
 /*
- * The issue is that Base is not abstract and no override.
- *
  * https://stackoverflow.com/questions/6924754/return-type-covariance-with-smart-pointers
  *
  * */
 
 
-/* Use covariance on raw pointers, and then wrap them */
+/* Use covariance on raw pointers, and then wrap them
+ * doClone() is overridden. Clone() is not.
+ *  */
 
-#ifdef SOLUTION2
+#ifdef SOLUTION3_1
 
+struct Base{
+
+private:
+	virtual Base* doClone() const {return new Base(*this); }
+
+public:
+	shared_ptr<Base> Clone() const {
+		Base* d = this->doClone();
+		std::shared_ptr<Base> p = std::make_shared<Base>(*d);
+		delete d;
+		return p;
+	}
+
+	virtual ~Base(){}
+};
+
+struct Derived : Base{
+
+private:
+	virtual Derived* doClone() const override { return new Derived(*this); }
+
+public:
+	shared_ptr<Derived> Clone() const {
+		Derived* d = this->doClone();
+		std::shared_ptr<Derived> p = std::make_shared<Derived>(*d);
+		delete d;
+		return p;
+	}
+};
+
+
+#elif defined SOLUTION3_2
+
+/*
+ * Simulate covariance by casting.
+ * doClone() is overridden. Clone() is not.
+ * */
 struct Base
 {
 private:
-   virtual Base* doClone() const {return new Base(*this); }
+	virtual shared_ptr<Base> doClone() const { return make_shared<Base>(*this); }
 
 public:
-   shared_ptr<Base> Clone() const { return shared_ptr<Base>(doClone()); }
+	shared_ptr<Base> Clone() const { return doClone(); }
 
-   virtual ~Base(){}
+	virtual ~Base(){}
 };
 
 struct Derived : Base
 {
 private:
-   virtual Derived* doClone() const { return new Derived(*this); }
+	virtual shared_ptr<Base> doClone() const override { return make_shared<Derived>(*this);  }
 
 public:
-   shared_ptr<Derived> Clone() const { return shared_ptr<Derived>(doClone()); }
-};
-
-
-#elif defined SOLUTION3
-
-/*Simulate covariance by casting*/
-struct Base
-{
-private:
-   virtual shared_ptr<Base> doClone() const { return make_shared<Base>(*this); }
-
-public:
-   shared_ptr<Base> Clone() const { return doClone(); }
-
-   virtual ~Base(){}
-};
-
-struct Derived : Base
-{
-private:
-   virtual shared_ptr<Base> doClone() const { return make_shared<Derived>(*this);  }
-
-public:
-   shared_ptr<Derived> Clone() const { return static_pointer_cast<Derived>(doClone()); }
+	shared_ptr<Derived> Clone() const { return static_pointer_cast<Derived>(doClone()); }
 };
 
 #endif
