2be2926768dfd839d708727cd8cb9d6e0087de12
yaojiadong <yaojiadong@gmail.com>
2022-08-02 13:32:23 +0200

    Add test case
    Pass one decorator to another decorator

--- design_pattern/decorator/test_decorator.cpp
+++ design_pattern/decorator/test_decorator.cpp
@@ -21,16 +21,22 @@ void test_decorator(){
 	fds->write_data(data);
 	fds->read_data();
 
-	// pass data source to decorator
-	Encrypt_decorator ed{fds};
-	Compression_decorator cd{fds};
-
 	// Operations on the data source with decoration, ie, encryption
+	std::cout << "\nEncrypt data..." <<std::endl;
+	Encrypt_decorator ed{fds};
 	ed.write_data(data);
 	ed.read_data();
+	
 	// Operations on the data source with decoration, ie, compression
+	std::cout << "\nCompress data..." <<std::endl;
+	Compression_decorator cd{fds};
 	cd.write_data(data);
 	cd.read_data();
-
-
+	
+	// Operations on the data source with decoration, ie, compression and encrypt
+	std::cout << "\nCompress and encrypt data..." <<std::endl;
+ 	std::shared_ptr<IData_source> comp_enc_deco = std::make_shared<Compression_decorator>(std::make_shared<Encrypt_decorator>(ed));
+// 	std::shared_ptr<IData_source> comp_enc_deco = std::make_shared<Compression_decorator>(std::make_shared<Encrypt_decorator>(fds));
+	comp_enc_deco->write_data(data);
+	comp_enc_deco->read_data();
 }
